7707608117d4299bae27351324b7a0399bcf9aff
daisuke-fukuda <32258949+daisuke-fukuda@users.noreply.github.com>
2020-06-24 22:21:53 +0900

    remove unnecessary code

--- extension/src/js/content.js
+++ extension/src/js/content.js
@@ -3,32 +3,16 @@ const exe = () => {
   console.log('[start]github-kusa-change');
 
   const kusaList = getKusaList();
-  console.log(kusaList);
 
   for (let i = 0; i < kusaList.length; i++) {
     changeKusaColor(kusaList[i]);
   }
 
   const lenengList = getLegendList();
-  console.log(lenengList);
   for (let i = 0; i < lenengList.length; i++) {
     changeLengendColor(lenengList[i]);
   }
 
-  // 末端の要素のtextを置換
-  // const { body } = document;
-  // getChildrenAndReplace(body);
-
-  // 全部置換しても大丈夫そうな要素
-  // replaceByTagName('span');
-  // replaceByTagName('h1');
-  // replaceByTagName('h2');
-  // replaceByTagName('h3');
-  // replaceByTagName('h4');
-  // replaceByTagName('a');
-  // replaceByTagName('p');
-  replaceByTagName('div');
-  replaceImg();
   // eslint-disable-next-line no-console
   console.log('[end]github-kusa-change');
 };
@@ -86,9 +70,8 @@ const changeKusaColor = (kusa) => {
 };
 
 const changeLengendColor = (legend) => {
-  // attributesのfillは昔のcolorがそのまま入っていて、
-  // 新旧のcolorをmappingしたscssで色が変わっている
-  // なので、styleでcolorを設定してあげればscssより優先されるので元の色に戻る
+  // styleで元々の色が入っているのを、cssで!importandで無理やり新しい色にしている
+  // ので、元のstyleを !importantにしてあげれば、cssより優先されて元の色になる、
   const color = legend.style.backgroundColor;
   legend.style.setProperty('background-color', color, 'important');
 
@@ -96,69 +79,6 @@ const changeLengendColor = (legend) => {
   legend.style.width = '11px';
   legend.style.height = '11px';
   legend.style.borderRadius = '0';
-};
-
-const replaceImg = () => {
-  const imgs = document.getElementsByTagName('img');
-  // eslint-disable-next-line no-restricted-syntax
-  for (const img of imgs) {
-    img.src = getImgUrl();
-  }
-  const sources = document.getElementsByTagName('source');
-  // eslint-disable-next-line no-restricted-syntax
-  for (const source of sources) {
-    source.srcset = getImgUrl();
-  }
-};
-const getImgUrl = () => chrome.extension.getURL(`cat${randRange(1, 5).toString()}.jpg`);
-// https://qiita.com/uto-usui/items/7193db237175ba15aaa3
-const randRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
-const replaceByTagName = (tag) => {
-  const elements = document.getElementsByTagName(tag);
-  // eslint-disable-next-line no-restricted-syntax
-  for (const element of elements) {
-    replaceText(element);
-  }
-};
-const getChildrenAndReplace = (node) => {
-  if (node.children.length === 0) {
-    replaceText(node);
-  } else {
-    const { children } = node;
-    // eslint-disable-next-line no-restricted-syntax
-    for (const child of children) {
-      getChildrenAndReplace(child);
-    }
-  }
-};
-
-const replaceText = (e) => {
-  // 末尾nodeだけ
-  if (e.innerHTML) {
-    let text = e.innerHTML;
-    text = text.replace(/コロナウイルス/g, '猫ちゃん');
-    text = text.replace(/コロナ/g, '子猫');
-    text = text.replace(/ウイルス/g, '🐈');
-    text = text.replace(/肺炎/g, '肉球');
-    text = text.replace(/新型/g, '今までより可愛い');
-    text = text.replace(/死亡/g, '😺可愛い');
-    text = text.replace(/死者/g, '🐈好き');
-    text = text.replace(/無くなった/g, '🐈好きになった');
-    text = text.replace(/感染者/g, '猫好きな人');
-    text = text.replace(/感染/g, '猫好き');
-    text = text.replace(/トイレットペーパー/g, 'キャットフード');
-    text = text.replace(/ティッシュペーパー/g, 'カリカリ');
-    text = text.replace(/マスク/g, 'またたび');
-    text = text.replace(/重篤/g, '猫しか見えない');
-    text = text.replace(/患者/g, 'ネコ好き');
-    text = text.replace(/重体/g, '重度の猫好き');
-    text = text.replace(/軽症/g, '猫が気になってきて');
-    text = text.replace(/意識不明/g, '可愛すぎて');
-    text = text.replace(/濃厚接触した/g, 'ネコとべたべた');
-    text = text.replace(/接触/g, 'ネコにもふもふ');
-    e.innerHTML = text;
-  }
 };
 
 // popupからのmessage
